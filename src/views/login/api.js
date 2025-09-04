9964410ad06de0a59613c521868922f373a7aa3d
voipke <voipke@qq.com>
2025-09-04 15:30:18 +0800

    连接本地isme-nest-serve测试修改

--- src/views/login/api.js
+++ src/views/login/api.js
@@ -10,6 +10,6 @@ import { request } from '@/utils'
 
 export default {
   toggleRole: data => request.post('/auth/role/toggle', data),
-  login: data => request.post('/auth/login', data, { needToken: false }),
+  login: data => request.post('/auth/login', data, { needToken: true, withCredentials: true }),
   getUser: () => request.get('/user/detail'),
 }
